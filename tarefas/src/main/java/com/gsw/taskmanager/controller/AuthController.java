96fa7bccfc2a8530b1b5a08bdb14927b0a7233dc
Pedro Alves <133861580+pphvaz@users.noreply.github.com>
2025-10-20 10:33:25 -0300

    fix[CJ-3]: Correção de controller Auth para enviar o id do usuario

--- tarefas/src/main/java/com/gsw/taskmanager/controller/AuthController.java
+++ tarefas/src/main/java/com/gsw/taskmanager/controller/AuthController.java
@@ -2,12 +2,16 @@ package com.gsw.taskmanager.controller;
 
 import com.gsw.taskmanager.dto.LoginRequest;
 import com.gsw.taskmanager.dto.TokenResponse;
+import com.gsw.taskmanager.entity.Usuario;
+import com.gsw.taskmanager.exception.BusinessException;
 import com.gsw.taskmanager.service.JwtService;
 import com.gsw.taskmanager.service.TokenService;
 import com.gsw.taskmanager.service.UsuarioService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -29,8 +33,8 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<TokenResponse> login(@RequestBody @Valid LoginRequest request) {
-        String token = usuarioService.autenticar(request);
-        return ResponseEntity.ok(new TokenResponse(token));
+        TokenResponse token = usuarioService.autenticar(request);
+        return ResponseEntity.ok(token);
     }
 
     @PostMapping("/logout")
