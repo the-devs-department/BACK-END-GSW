8d3892b586169fe39868a0b6dada4126c4a8c8eb
Gustavo Almeida Camargo <88459386+GustavoAC0802@users.noreply.github.com>
2025-11-22 20:37:49 -0300

    Criação da estrutura inicial de equipe

---
+++ service-equipe/src/main/java/com/gsw/service_equipe/ServiceEquipeApplication.java
@@ -0,0 +1,13 @@
+package com.gsw.service_equipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceEquipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceEquipeApplication.class, args);
+	}
+
+}
